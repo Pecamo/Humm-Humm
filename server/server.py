71d7491afc04cd9ef0524ccaaab7c37100590423
Jérémy Rabasco <rabasco.jeremy@gmail.com>
2015-04-18 19:47:51 +0200

    server.py

--- server/server.py
+++ server/server.py
@@ -3,22 +3,70 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
 import json
+import os
+from os import curdir, sep
+
+CWD = os.path.abspath('.')
 
 
 hostName = "localhost"
 hostPort = 31415
 
 
+def make_index(relative_path):
+
+    absolute_path = os.path.abspath(relative_path)
+    file_list = os.listdir(absolute_path)
+
+    relative_list = []
+    for file_name in file_list:
+        relative_name = os.path.join(relative_path, file_name)
+        relative_list.append(relative_name)
+
+    html_list = []
+    for r in relative_list:
+        r = r + "/" if os.path.isdir(r) else r
+        html_list.append('<a href="http://localhost:31415/%s">%s</a><br>' % (r, r))
+
+    page_template = "<html><head></head><body>%s</body></html>"
+
+    ret = page_template % ('\n'.join(html_list))
+
+    return ret
+
+
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
-        query = urlparse(self.path).query
-        parsed_args = parse_qs(query)
-        cleaned_path = urlparse(self.path).path
-        print(parsed_args)
-        self.send_html("<html><body><h3>SALUT !</h3></body></html>")
+        try:
+
+            if self.path.endswith('/'):
+                relative_path = self.path[1:]
+                relative_path = relative_path if len(relative_path) > 0 else '.'
+                page = make_index(relative_path)
+                self.send_html(page)
+
+            elif self.path.endswith(".html"):
+                f = open(curdir + sep + self.path, 'r')
+                self.send_html(f.read())
+                f.close()
+            else:
+
+                filepath = self.path[1:]
+
+                f = open( os.path.join(CWD, filepath), "rb")
+
+                self.send_file(f.read())
+                f.close()
+
+        except IOError as e :
+            print(e)
+            self.send_error(404,'File Not Found: %s' % self.path)
 
     def send_html(self, html: str):
         self.send(200, [("Content-type", "text/html")], bytes(html, encoding="utf -8"))
+
+    def send_file(self, file: bytes):
+        self.send(200, [("Content-type", "application/octet-stream")], file)
 
     def send(self, code: int, headers: [(str, str)],  data: bytes):
         self.send_response(code)
