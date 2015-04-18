b8be813584665e6cae22a02d364f62b339e33061
Jérémy Rabasco <rabasco.jeremy@gmail.com>
2015-04-19 01:34:46 +0200

    uploads

--- server/helpers.py
+++ server/helpers.py
@@ -8,6 +8,9 @@ class ResType(Enum):
     CSS = "css"
     FILE = "file"
     ERROR = "error"
+    SOUND = "sound"
+    PLAYER = "player"
+
 
 def bytes_json_from_dict(data: dict):
     return bytes(json.dumps(data), encoding="utf-8")
@@ -20,7 +23,19 @@ def get_res_type(path: str) -> ResType:
         res_type = ResType.CSS
     elif path.endswith(".js"):
         res_type = ResType.JS
+    elif path.split("/")[1] == "sounds":
+        res_type = ResType.SOUND
+    elif path.split("/")[1] == "player":
+        res_type = ResType.PLAYER
     else:
         res_type = ResType.FILE
 
     return res_type
+
+
+def get_player_for_sound(ref: str) -> str:
+        with open("sound_player.tpl", "r") as player:
+            whole = player.read()
+            splits = whole.split("$$")
+            splits[1] = ref
+        return "".join(splits)
