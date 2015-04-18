fe906031aced29e2f68c200f2168bc84eceb71e8
Flagoul <basile.vu@gmail.com>
2015-04-18 23:00:04 +0200

    Fixing buttons

--- view.js
+++ view.js
@@ -9,3 +9,38 @@ $('.post').on('click', function () {
 	var win = window.open('https://www.reddit.com/r/HummHumm', '_blank');
 	win.focus();
 });
+
+
+// Audio capture button actions
+$(document).ready(function(){
+	$('#download').addClass('disabled');
+	
+	$(document).on("click", "#record:not(.disabled)", function(){
+		$.voice.record(false, function(){
+			$('#record').addClass("disabled");
+			$("#stop, #download").removeClass("disabled");
+			$('#humming-analysis').text('Recording...');
+		});
+	});
+	
+	$(document).on("click", "#stop:not(.disabled)", function(){
+		$.voice.export(function(url){
+			$("#audio").attr("src", url);
+		}, "URL");
+		endRecord();
+	});
+	
+	$(document).on("click", "#download:not(.disabled)", function(){
+		$.voice.export(function(url){
+			$("<a href='"+url+"' download='Recording.wav'></a>")[0].click();
+		}, "URL");
+		endRecord();
+	});
+});
+
+function endRecord() {
+	$("#record").removeClass("disabled");
+	$("#download").addClass("disabled");
+	$.voice.stop();
+	$('#humming-analysis').text('');
+}
