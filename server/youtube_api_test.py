1de98ffe43c60ec367447bec315dd54fb2e162e6
Jérémy Rabasco <rabasco.jeremy@gmail.com>
2015-04-19 09:07:36 +0200

    test youtube

--- server/youtube_api_test.py
+++ server/youtube_api_test.py
@@ -11,10 +11,7 @@ youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVE
 
 search_response = youtube.search().list(q="darude sandstorm", part="id,snippet", maxResults=10).execute()
 
-videos = []
 
 for search_result in search_response.get("items", []):
     if search_result["id"]["kind"] == "youtube#video":
-        videos.append("%s (%s)" % (search_result["snippet"]["title"], search_result["id"]["videoId"]))
-
-print("Videos:\n", "\n".join(videos), "\n")
+        print(search_result)
