8494ee965c1d913735b54c3dd4fffdc3e29cc941
Kewin Dousse <kewin.d@websud.ch>
2015-04-19 05:20:02 +0200

    Adding three functions to handle captchas

--- reddit-connect.js
+++ reddit-connect.js
@@ -19,3 +19,34 @@ function postLink(link, titleText) {
 		console.log(data);
 	});
 }
+
+function needsCaptcha() {
+	reddit.auth(accessToken).then(function() {
+		return reddit('/api/needs_captcha').get();
+	}).then(function(data) {
+		console.log("After POST : ");
+		console.log(data);		
+	});
+}
+
+function newCaptcha() {
+	reddit.auth(accessToken).then(function() {
+		return reddit('/api/new_captcha').post({
+			api_type: "json"
+		});
+	}).then(function(data) {
+		console.log("After POST : ");
+		console.log(data);		
+	});
+}
+
+function captchaIden(identifier) {
+	reddit.auth(accessToken).then(function() {
+		return reddit('/captcha/$iden').get({
+			$iden: identifier
+		});
+	}).then(function(data) {
+		console.log("After POST : ");
+		console.log(data);		
+	});
+}
