6087f0046eecd20e1f25861e2a9258f97d4fe920
Sacha Bron <sacha.bron@copperflake.com>
2015-04-19 13:23:21 +0200

    video integration

--- post.js
+++ post.js
@@ -33,7 +33,15 @@ $(function() {
 
 		for (var i = 0, l = comments.length; i < l; i++) {
 			var c = comments[i].data;
-			console.log(c.replies)
+			var comments2 = c.replies.data.children;
+			var youtubeId;
+
+			for (var j = 0, k = comments2.length; j < k; j++) {
+				var cc = comments2[j];
+				if (cc.author === "humm_youtube") {
+					youtubeId = str.match(/\[(.*)\]\((.*)\)/)[2].match(/watch\?v=(.*)/)[1];
+				}
+			}
 			
 			var decoded = $("<div/>").html(c.body_html).text();
 
@@ -41,8 +49,13 @@ $(function() {
 			'<div class="post">' +
 				'<div class="body">' +
 					decoded +
-				'</div>' +
-				'<div class="footer">' +
+				'</div>';
+
+				if (typeof youtubeId !== 'undefined') {
+					html += '<iframe width="420" height="345" src="http://www.youtube.com/embed/' + youtubeId + '"></iframe>';
+				}
+
+				html += '<div class="footer">' +
 					'<small>' +
 						'<a href="https://www.reddit.com/user/' + c.author + '">' +
 							c.author +
